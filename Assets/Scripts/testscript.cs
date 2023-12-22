a446fd6322ae5a8922291ca265385f0937d0d84b
Mustafa <94108685+ustadmustafa@users.noreply.github.com>
2023-12-22 20:24:30 +0300

    deneme 1

---
+++ Assets/Scripts/testscript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("deneme1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
