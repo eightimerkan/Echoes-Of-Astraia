92615316b1d27f1b4ee41e31d925d32892fb08bb
eightimerkan <erkan.dolgun2@gmail.com>
2024-01-01 21:04:57 +0300

    ControllingThePlayer

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveInput;
+
+    public Rigidbody2D theRB;
+    public Transform gunArm;
+    private Camera theCam;
+
+    public Animator anim;
+    void Start()
+    {
+        theCam = Camera.main;
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        //transform.position += new Vector3(moveInput.x, moveInput.y, 0f) * moveSpeed * Time.deltaTime;
+        theRB.velocity = moveInput * moveSpeed;
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        if(mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
