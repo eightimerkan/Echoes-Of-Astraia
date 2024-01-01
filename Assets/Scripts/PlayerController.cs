09d3957ed493a0ac045a2a1ec48316f832bcd9c4
eightimerkan <erkan.dolgun2@gmail.com>
2024-01-01 22:20:24 +0300

    Shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,11 @@ public class PlayerController : MonoBehaviour
     private Camera theCam;
 
     public Animator anim;
+    public GameObject bulletToFire;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotCounter;
+
     void Start()
     {
         theCam = Camera.main;
@@ -44,6 +49,21 @@ public class PlayerController : MonoBehaviour
         Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0)
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                shotCounter = timeBetweenShots;
+            }
+        }
 
         if(moveInput != Vector2.zero)
         {
